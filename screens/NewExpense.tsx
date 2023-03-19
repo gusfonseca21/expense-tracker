fc92451e2779855d7b5a96a8511c3bb7f97b9269
Gustavo Fonseca <gusfonseca.dev@gmail.com>
2023-03-19 00:45:07 -0300

    Criando tela de nova despesa

--- screens/NewExpense.tsx
+++ screens/NewExpense.tsx
@@ -1,11 +1,71 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, View, StyleSheet, TextInput, Button } from "react-native";
+import { useState } from "react";
+
+import MaskInput, { Masks } from "react-native-mask-input";
+
+// Styles
 import { globalStyles } from "../global/styles";
+import { palette } from "../global/styles";
 
 export default function NewExpense() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  function sendExpense() {
+    console.log("first");
+  }
+
   return (
-    <View style={globalStyles.pageStyle}>
-      <Text>NewExpense</Text>
+    <View style={[globalStyles.pageStyle, { paddingHorizontal: 20 }]}>
+      <TextInput
+        style={styles.input}
+        placeholder='Título'
+        onChangeText={setName}
+        value={name}
+      />
+      <MaskInput
+        style={styles.input}
+        placeholder='Valor'
+        onChangeText={(_, unmasked) => setAmount(unmasked)}
+        inputMode='numeric'
+        value={amount}
+        mask={Masks.BRL_CURRENCY}
+      />
+      <MaskInput
+        style={styles.input}
+        placeholder='Data'
+        onChangeText={setDate}
+        inputMode='numeric'
+        value={date}
+        mask={Masks.DATE_DDMMYYYY}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Descrição'
+        onChangeText={setDescription}
+        value={description}
+        multiline
+      />
+      <Button
+        onPress={sendExpense}
+        title='Adicionar Despesa'
+        color='#841584'
+        accessibilityLabel='Learn more about this purple button'
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: palette.secondary.main,
+    padding: 10,
+    marginBottom: 15,
+    fontSize: 20,
+  },
+});
